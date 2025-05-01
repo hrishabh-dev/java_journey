ba5c6a0bb82d16efbb73087afe4685416f78e9ca
hrishabh-dev <hrishabh068mg@example.com>
2025-05-01 12:15:19 +0530

    commits

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the computer\n I am learning java");
+        System.out.print("HI This is my first program");
+    }
+}
---
+++ variables.java
@@ -0,0 +1,17 @@
+public class variables {
+    public static void main(String[] args) {
+        System.out.println("Variables ");
+        int a=10;
+        double price=20.24;
+        String name="Tim";
+        System.out.println(a);
+        System.out.println(name);
+        // we can change values even after defining 
+        a=20;
+        name="Trick";
+        System.out.println("After changing values -------------------");
+        System.out.println(a);
+        System.out.println(name);
+    }
+    
+}
