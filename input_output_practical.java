3abb1761c8d1989492997f3f9574dd84982ba230
hrishabh-dev <hrishabh068mg@example.com>
2025-05-02 07:16:25 +0530

    practical of input_ouput

---
+++ input_output_practical.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class input_output_practical {
+    public static void main(String[] args) {
+        System.out.println("addition using user input");
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int sum=a+b;
+        System.out.println("-----sum is---------");
+        System.out.println(sum);
+
+    }
+    
+}
