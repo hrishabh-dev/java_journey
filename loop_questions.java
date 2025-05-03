a3658d2d0980d3e425e9dd5795f002ee5d9faa04
hrishabh-dev <hrishabh068mg@example.com>
2025-05-03 19:18:22 +0530

    questions of loop

--- loop_questions.java
+++ loop_questions.java
@@ -4,13 +4,22 @@ public class loop_questions {
         //print sum of first n natural numbers
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number :-");
-        int n=sc.nextInt();
+        int s=sc.nextInt();
         int sum=0;
-        for (int i=1;i<=n;i++){
+        for (int i=1;i<=s;i++){
             sum=sum+i;
-
+            
         }
         System.out.println(sum);
+
+
+
+
+        //print table of a number input by the user 
+        int x=sc.nextInt();
+        for (int i=1;i<11;i++){
+            System.out.println(x*i);
+        }
 
     }
     
