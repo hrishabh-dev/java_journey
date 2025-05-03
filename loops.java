adf49e0823a84175672a5ed19bb746f12ba7a6dc
hrishabh-dev <hrishabh068mg@example.com>
2025-05-03 18:48:16 +0530

    for loop

---
+++ loops.java
@@ -0,0 +1,12 @@
+public class loops {
+    public static void main(String[] args) {
+        // using for loop checking even numbers between 0 to 21
+        for (int i=0;i<22;i=i+1){
+            if (i%2==0){
+                System.out.println(i);
+
+            }
+        }
+    }
+    
+}
