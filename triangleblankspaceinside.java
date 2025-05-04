60548f094a92db7738fc2bab3a89bf35a320cc5e
hrishabh-dev <hrishabh068mg@example.com>
2025-05-04 21:45:18 +0530

    updated

--- triangleblankspaceinside.java
+++ triangleblankspaceinside.java
@@ -1,17 +1,25 @@
 public class triangleblankspaceinside {
     public static void main(String[] args) {
         System.out.println("print rectangle with blank space inside");
-        for(int i=1;i<=5;i++){
-        for (int j=1;j<=7;j++){
-            if (i==1 || j==1 || j==7  || i==5){
-                System.out.print("*");
-            
-            }else{
-                System.out.print(" ");
+        for (int i=1;i<=5;i++){
+            for (int j=1;j<=5;j++){
+                if (i==1 && j==3){
+                    System.out.print("*");
+                }else if(i==2&&(j==2||j==4)){
+                    System.out.print("*");
+                }else if (i==3 && (j==1 || j==5)){
+                    System.out.print("*");
+                }else if (i==3 &&j==3 ){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+                
+                
             }
+            System.out.println();
         }
-        System.out.println();
-       }
     }
     
 }
