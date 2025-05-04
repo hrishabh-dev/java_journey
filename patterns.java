9b5facc31602160341ba9659b00c9ed39d0ff365
hrishabh-dev <hrishabh068mg@example.com>
2025-05-04 21:16:06 +0530

    newpatterns

--- patterns.java
+++ patterns.java
@@ -41,6 +41,23 @@ public class patterns {
         }
         System.out.println();
         }   */
+       /*System.out.println("Right angle triangle");
+       for (int i=1;i<=4;i++){
+        for (int j=1;j<=4;j++){
+            if (i==1 && j==1){
+                System.out.print("*");
+            }else if (i==2 && (j==1||j==2)){
+                System.out.print("*");
+            }else if (i==3&&(j==1||j==2||j==3)){
+                System.out.print("*");
+            }else if (i==4&&(j==1||j==2||j==3||j==4)){
+                System.out.print("*");
+            }else{
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+       }*/
     }
     
 }
