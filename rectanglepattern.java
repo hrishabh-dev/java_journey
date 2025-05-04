0570e9b680aa3815e7acfabdcbea896a1abb6197
hrishabh-dev <hrishabh068mg@example.com>
2025-05-04 21:27:36 +0530

    rectangle

---
+++ rectanglepattern.java
@@ -0,0 +1,12 @@
+public class rectanglepattern {
+    public static void main(String[] args) {
+        System.out.println("Printing star pattern in rectangle shape");
+        for (int i=1;i<=4;i++){
+            for (int j=1;j<=8;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
